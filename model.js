c1a63f894afa557baad8cb71002ca1a23b0f632c
Shrusti <89652827+shrusticodes@users.noreply.github.com>
2023-04-24 16:57:54 +0530

    local storage used instead of array

--- model.js
+++ model.js
@@ -1,37 +1,38 @@
 export class Model{
-    constructor(){
-     this.toDoList=[
-      {id:81, task:"Plant a tree", check:false},
-      {id:278, task:"Pick up the papers",check:true},
-      {id:335, task:"Run 100m", check:false},
-      {id:74, task:"Water the plants", check:false},
-      {id:95, task:"Clean the house", check:true}]
-    };
-    addItem(currentTask)
-    {
-      let toDoObject={id:Math.floor(Math.random() * 1000 + 1),task:currentTask,check:false};
+  constructor(){
+      this.toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+  }
+  
+  _commit(toDoList) {
+      this.onTodoListChanged(toDoList)
+      localStorage.setItem('toDoList', JSON.stringify(toDoList))
+  }
+  
+  addItem(currentTask) {
+      let toDoObject = { id: Math.floor(Math.random() * 1000 + 1), task: currentTask, check: false };
       this.toDoList.push(toDoObject);
-      this.getTaskList(this.toDoList);
-    }
-    editTask(id,updatedTask)
-    {
-        let index = this.toDoList.findIndex((task) => task.id === id);
-        this.toDoList[index].task=updatedTask;
-        this.getTaskList(this.toDoList);
-    }
-    deleteTask(id)
-    {
-        let index = this.toDoList.findIndex((task) => task.id === id);
-        this.toDoList.splice(index, 1);
-        this.getTaskList(this.toDoList);
-    }
-    checkedTask(id)
-    {
-        let index=this.toDoList.findIndex((task) => task.id === id);
-        this.toDoList[index].check=!this.toDoList[index].check;
-        this.getTaskList(this.toDoList);
-    }
-    passToDoListChanges(callback){
-      this.getTaskList=callback;
-    }
+      this._commit(this.toDoList);
+  }
+  
+  editTask(id, updatedTask) {
+      let index = this.toDoList.findIndex((task) => task.id === id);
+      this.toDoList[index].task = updatedTask;
+      this._commit(this.toDoList);
+  }
+  
+  deleteTask(id) {
+      let index = this.toDoList.findIndex((task) => task.id === id);
+      this.toDoList.splice(index, 1);
+      this._commit(this.toDoList);
+  }
+  
+  checkedTask(id) {
+      let index = this.toDoList.findIndex((task) => task.id === id);
+      this.toDoList[index].check = !this.toDoList[index].check;
+      this._commit(this.toDoList);
+  }
+  
+  passToDoListChanges(callback) {
+      this.onTodoListChanged = callback;
+  }
 }
--- view.js
+++ view.js
@@ -1,94 +1,95 @@
-export class View{
-    constructor(){
-        this.form=this.createHTMLElement('form');
-        this.input=this.createHTMLElement('input');
-        this.input.type="text";
-        this.input.name="task";
-        this.submitButton=this.createHTMLElement('button');
-        this.submitButton.textContent="SUBMIT";
-        this.taskList=this.createHTMLElement('ul');
-        this.form.append(this.input,this.submitButton);
-        this.getHTMLElement('#root').append(this.form,this.taskList);
+export class View {
+    constructor() {
+        this.form = this.createHTMLElement('form');
+        this.input = this.createHTMLElement('input');
+        this.input.type = "text";
+        this.input.name = "task";
+        this.submitButton = this.createHTMLElement('button');
+        this.submitButton.textContent = "SUBMIT";
+        this.taskList = this.createHTMLElement('ul');
+        this.form.append(this.input, this.submitButton);
+        this.getHTMLElement('#root').append(this.form, this.taskList);
         this.temporaryToDo;
     }
-    createHTMLElement(tagName,className)
-    {
+    
+    createHTMLElement(tagName, className) {
         let element = document.createElement(tagName);
-        if(className) element.className=className;
+        if (className) element.className = className;
         return element;
     }
-    getHTMLElement(selector)
-    {
+    
+    getHTMLElement(selector) {
         let element = document.querySelector(selector);
         return element;
     }
+    
     get todoText() {
         return this.input.value;
-      }
+    }
+    
     resetInput() {
         this.input.value = '';
     }
-    displayElements(toDoList)
-    {
+    
+    displayElements(toDoList) {
         while (this.taskList.firstChild) {
-            this.taskList.removeChild(this.taskList.firstChild)
-          }     
-        toDoList.forEach((task)=>{
-        let li=this.createHTMLElement('li');
-        li.id=task.id;
-        let checkbox=this.createHTMLElement('input');
-        checkbox.type="checkbox";
-        checkbox.checked=task.check;
-        let span=this.createHTMLElement('span','editable');
-        span.textContent=task.task;
-        span.contentEditable=true;
-        let deleteButton=this.createHTMLElement('button','delete');
-        deleteButton.textContent="DELETE";
-        li.append(checkbox,span,deleteButton);
-        this.taskList.append(li);});
-        console.log(toDoList);
+            this.taskList.removeChild(this.taskList.firstChild);
+        }
+        
+        toDoList.forEach((task) => {
+            let li = this.createHTMLElement('li');
+            li.id = task.id;
+            let checkbox = this.createHTMLElement('input');
+            checkbox.type = "checkbox";
+            checkbox.checked = task.check;
+            let span = this.createHTMLElement('span', 'editable');
+            span.textContent = task.task;
+            span.contentEditable = true;
+            let deleteButton = this.createHTMLElement('button', 'delete');
+            deleteButton.textContent = "DELETE";
+            li.append(checkbox, span, deleteButton);
+            this.taskList.append(li);
+        });
     }
-    passAddTaskValue(handler)
-    {
+    
+    passAddTaskValue(handler) {
         this.form.addEventListener('submit', (element) => {
             element.preventDefault();
         
             if (this.todoText) {
-              handler(this.todoText);
-              this.resetInput();
+                handler(this.todoText);
+                this.resetInput();
             }
-          });
+        });
     }
-    passDeleteTaskValue(handler)
-    {
+    
+    passDeleteTaskValue(handler) {
         this.taskList.addEventListener('click', (element) => {
             if (element.target.className === 'delete') {
-              const id = parseInt(element.target.parentElement.id);
-              handler(id);
+                const id = parseInt(element.target.parentElement.id);
+                handler(id);
+                }
+              });
             }
-          });
-    }
-    passCheckedTaskValue(handler)
-    {
-        this.taskList.addEventListener('change', (element) => {
-            if (element.target.type === 'checkbox') {
-              const id = parseInt(element.target.parentElement.id);
-              handler(id);
+            passCheckedTaskValue(handler) {
+              this.taskList.addEventListener('change', (element) => {
+              if (element.target.type === 'checkbox') {
+                const id = parseInt(element.target.parentElement.id);
+                handler(id);
+                }
+              });
             }
-          });  
-    }
-    passEditTaskValue(handler)
-    {
-      this.taskList.addEventListener("input",(element)=>{
-        if(element.target.className ==="editable") 
-        this.temporaryToDo = element.target.innerText;
-      });
-      this.taskList.addEventListener('focusout',(element)=>{
-        if(this.temporaryToDo){
-          let id = parseInt(element.target.parentElement.id);
-          handler(id,this.temporaryToDo);
-          this.temporaryToDo='';
-        }
-      });
-    }
- }
+           passEditTaskValue(handler) {
+              this.taskList.addEventListener('input', (element) => {
+              if (element.target.className === 'editable')
+              this.temporaryToDo = element.target.innerText;
+              });
+              this.taskList.addEventListener('focusout', (element) => {
+              if (this.temporaryToDo) {
+              let id = parseInt(element.target.parentElement.id);
+              handler(id, this.temporaryToDo);
+              this.temporaryToDo = '';
+              }
+            });
+          }
+      }
